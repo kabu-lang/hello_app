a2277d69d18a414c2c7e2532bc11bf942bd84a98
yuki <xzj32ejt8qsmr9tmx9xf@docomo.ne.jp>
2021-01-09 01:44:28 +0900

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
-  
+
   # 特定のワーカーではテストをパラレル実行する
   parallelize(workers: :number_of_processors)
 
@@ -16,4 +16,10 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # （すべてのテストで使うその他のヘルパーメソッドは省略）
+
+# テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
 end
