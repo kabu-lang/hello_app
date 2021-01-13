9c17eecae86eecb641fb99de821d580d306b3808
yuki <xzj32ejt8qsmr9tmx9xf@docomo.ne.jp>
2021-01-14 01:50:59 +0900

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,6 +24,11 @@ module SessionsHelper
       end
     end
 
+  def current_user?(user)
+    user && user == current_user
+  end
+
+
   def logged_in?
     !current_user.nil?
   end
@@ -39,4 +44,15 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
-end
+
+  # 記憶したURL（もしくはデフォルト値）にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+  end
