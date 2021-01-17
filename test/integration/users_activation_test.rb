e9cf4da798b89c7df16731842ba93a3d3d22c01c
yuki <xzj32ejt8qsmr9tmx9xf@docomo.ne.jp>
2021-01-17 10:43:49 +0900

    Add account activation

---
+++ test/integration/users_activation_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UsersActivationTest < ActionDispatch::IntegrationTest
+
+def setup
+  @user = users(:michael)
+  @other_user = users(:archer)
+  @non_active_user = users(:Kati)
+end
+
+test "index only activated user" do
+  log_in_as(@user)
+  get users_path
+  assert_select 'a[href=?]',user_path(@other_user)
+  assert_select 'a[href=?]',user_path(@non_active_user), count:0
+end
+
+test "show only activated user" do
+  log_in_as(@other_user)
+  get user_path(@user)
+  assert_template 'users/show'
+  get user_path(@non_active_user)
+  assert_redirected_to root_url
+
+end
+
+
+
+end
